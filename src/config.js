017375040df464652ac2cf05712c64f21aefbc71
Shane Clifford <shane.clifford@analog.com>
2020-06-09 00:53:33 +0100

    Increasing retry attempts and duration to address rate throttling on CF.describeStack

--- src/config.js
+++ src/config.js
@@ -75,8 +75,8 @@ let baseConfig = {
     delete this.AWS;
     this.AWS = require('aws-sdk');
 
-    this.AWS.config.maxRetries = 7;
-    this.AWS.retryDelayOptions = {base: Math.random() * (3000 - 900) + 900};
+    this.AWS.config.maxRetries = 12;
+    this.AWS.retryDelayOptions = {base: Math.random() * (4000 - 800) + 800};
 
     return this.AWS;
   },
